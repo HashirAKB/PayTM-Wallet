8d16eb3bfbc15edc56af21ca14a06c2c29f6409b
HashirAKB <ahmedhashir96@gmail.com>
2024-08-07 15:34:49 +0530

    Added cors, body parser and jsonwebtoken.

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,12 @@
 // backend/index.js
 const express = require("express");
 const rootRouter = require('./api/index');
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
 app.use("/api/v1", rootRouter);
+app.listen(3000);
