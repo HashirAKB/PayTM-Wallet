c70a5a5b172efeb8ede42c5517b10f35dc06beda
HashirAKB <ahmedhashir96@gmail.com>
2024-08-08 13:45:47 +0530

    frontend skeleton

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,22 @@
+import { SignIn } from "./pages/signin"
+import { SignUp } from "./pages/signup"
+import { Dashboard } from "./pages/dashboard"
+import { SendMoney } from "./pages/sendmoney"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
 
   return (
-    <div>
-        Hello world
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<SignUp />}/>
+          <Route path="/signin" element={<SignIn />}/>
+          <Route path="/dashboard" element={<Dashboard />}/>
+          <Route path="/send" element={<SendMoney />}/>
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
